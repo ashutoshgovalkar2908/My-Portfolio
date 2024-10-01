a0c2c95f42b00d21639665bebde42837eeec6162
ashutoshgovalkar2908 <ashutoshgovalkar942@gmail.com>
2024-10-01 13:50:48 +0530

    event handler and functions for form validation

--- portfolio.js
+++ portfolio.js
@@ -55,3 +55,54 @@
         dwnArw.classList.remove('rotate-arrow')
      }
  })
+
+ // Form Validation
+
+    document.getElementById("contForm").addEventListener('submit',(event)=>{
+    
+    // Prevent form submission
+    event.preventDefault(); 
+
+    // Clear previous error messages
+    let errorMessage = '';
+    document.getElementById("errorMessage").textContent = '';
+
+    // Get form values
+    const fname = document.getElementById("fullname").value.trim();
+    const femail = document.getElementById("email").value.trim();
+    const fmessage = document.getElementById("message").value.trim();
+
+    // Regular expression to check if email format is valid
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
+    // Regular expression to check if name contains only alphabetic characters (and spaces)
+    const namePattern = /^[a-zA-Z\s]+$/;
+
+    //validate name
+    if(fname === ''){
+        errorMessage += 'Please enter your name.\n';
+    }else if(!namePattern.test(fname)){
+        errorMessage += '*Name should only contain letters.\n';
+    }
+
+    // validate email
+    if(femail === ''){
+        errorMessage += 'Please enter your email.\n';
+    }else if(!emailPattern.test(femail)){
+        errorMessage += '*Please enter a valid email address.\n';
+    }
+
+    //validate message
+    if(fmessage === ''){
+        errorMessage += '*Please enter your message.\n';
+    }
+
+    // if there are error show them
+    if(errorMessage){
+        document.getElementById("errorMessage").textContent = errorMessage;
+    }else{
+        // If no errors, submit the form or process the data
+        alert('Form Submitted Succesfully');
+    }
+   
+});
